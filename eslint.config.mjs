aca5556465ca120f9da57732605a4ec71f74441b
Dayron_Glez <112677796+Dayron-Glez@users.noreply.github.com>
2026-05-18 01:45:10 +0200

    chore(eslint): switch to typescript-eslint flat config, fix circular ref

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,12 @@
-import { dirname } from "path"
-import { fileURLToPath } from "url"
-import { FlatCompat } from "@eslint/eslintrc"
+import js from "@eslint/js"
+import tseslint from "typescript-eslint"
+import eslintConfigPrettier from "eslint-config-prettier"
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
-
-const compat = new FlatCompat({ baseDirectory: __dirname })
-
-export default [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  ...compat.extends("prettier"),
-]
+export default tseslint.config(
+  js.configs.recommended,
+  tseslint.configs.recommended,
+  eslintConfigPrettier,
+  {
+    ignores: [".next/**", "node_modules/**", ".source/**"],
+  }
+)
