3f3ee3ff6ccad5da4a6d2c56771599ae61d8209f
Dayron_Glez <112677796+Dayron-Glez@users.noreply.github.com>
2026-05-20 01:45:58 +0200

    feat(maturity): render MaturityBadge md in docs meta-bar

--- app/[lang]/docs/[[...slug]]/page.tsx
+++ app/[lang]/docs/[[...slug]]/page.tsx
@@ -1,4 +1,5 @@
 import { getPageImage, getPageMarkdownUrl, source } from "@/lib/source"
+import { MaturityBadge } from "@/components/MaturityBadge"
 import {
   DocsBody,
   DocsDescription,
@@ -31,6 +32,7 @@ export default async function Page(props: PageProps<"/[lang]/docs/[[...slug]]">)
       <DocsTitle>{page.data.title}</DocsTitle>
       <DocsDescription className="mb-0">{page.data.description}</DocsDescription>
       <div className="flex flex-row items-center gap-2 border-b pb-6">
+        <MaturityBadge maturity={page.data.maturity} size="md" />
         <MarkdownCopyButton markdownUrl={markdownUrl} />
         <ViewOptionsPopover
           markdownUrl={markdownUrl}
